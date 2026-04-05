341604814e1ef2eace253609f267391628d4569f
Praveen-R-2518 <ramanathan.praveen456@gmail.com>
2026-04-05 23:40:04 +0530

    Add EventsSectionNew component with event carousel and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import TextSphereAnimation from './components/TextSphereAnimation';
+import HeroSection from './components/HeroSection';
+import EventsSectionNew from './components/EventsSectionNew';
+import AboutSection from './components/AboutSection';
+import TeamSection from './components/TeamSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
 
 export default function Home() {
-  return <TextSphereAnimation />;
+  return (
+    <main className="flex-1">
+      <HeroSection />
+      <EventsSectionNew />
+      <AboutSection />
+      <TeamSection />
+      <ContactSection />
+      <Footer />
+    </main>
+  );
 }
+
